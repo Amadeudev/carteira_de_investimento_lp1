6bdfbf1e62fcebb7b97eb4573920d6786a11b0f7
Amadeu Fernandes <81340479+Amadeudev@users.noreply.github.com>
2025-07-14 15:11:32 -0300

    criando alocação inicial

--- main.c
+++ main.c
@@ -1,3 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define FATOR_ESCALA 100 //convertendo valor monetário em inteiro
+typedef struct {
+    char nome[20];
+    double custo;
+    double retorno;
+} Acao;
 
+int main() {
+    double capital_de_investimento;
+    int num_acoes_de_entrada;
+
+    printf("----------------------------------------\n");
+    printf("  Otimizacao de Carteira de Investimentos\n");
+    printf("----------------------------------------\n\n");
+
+    printf("Digite o capital de investimento: ");
+    scanf("%lf", &capital_de_investimento);
+    
+    printf("Digite o numero de ações de entrada: ");
+    scanf("%d", &num_acoes_de_entrada);
+
+    Acao *acoes_disponiveis = (Acao *)malloc(num_acoes_de_entrada * sizeof(Acao));
+    if (acoes_disponiveis == NULL) {
+        printf("Erro de alocacao de memoria para as ações.\n");
+        return 1;
+    }
+
+    return 0;
+}
